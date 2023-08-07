bde8a953d6492333d07e5400ae37018e01cc0a29
tharrry <martin.leger@outlook.de>
2023-08-07 22:53:12 +0200

    Added handlebars for HTML templating

--- homechef/src/main.ts
+++ homechef/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+  hbs.registerPartials(join(__dirname, '..', '/views/partials'));
+  
+
   await app.listen(3000);
 }
 bootstrap();
