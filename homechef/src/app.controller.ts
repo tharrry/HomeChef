e601db99b3958637f7313f853a2ac74b39db814a
tharrry <martin.leger@outlook.de>
2023-08-07 22:55:29 +0200

    Installed hbs

--- homechef/src/app.controller.ts
+++ homechef/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Header } from '@nestjs/common';
+import { Controller, Get, Header, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,8 +6,14 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Header('Content-Type', 'text/html')
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  root() {
+    return {message: 'Hello there!'}
   }
+
+  //@Get()
+  //@Header('Content-Type', 'text/html')
+  //getHello(): string {
+  //  return this.appService.getHello();
+  //}
 }
